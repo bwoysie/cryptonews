66e2567c027f14c6d1b263c89a2d818471bd922c
Patrick Harrison <jahman@bellatlantic.net>
2024-09-15 12:52:08 -0400

    updated api call

--- crypto/views.py
+++ crypto/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-  return render(request, 'home.html',{})
+  import requests
+  import json
+  api_request = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+  api = json.loads(api_request.content)
+  return render(request, 'home.html',{'api':api})
