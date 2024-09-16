354d13406e8cd4412d49d3b7e60ab64e98579c02
Patrick Harrison <jahman@bellatlantic.net>
2024-09-16 11:16:35 -0400

    loaded and cinfigured api data

--- crypto/views.py
+++ crypto/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def home(request):
   import requests
   import json
-  api_request = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+  api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
   api = json.loads(api_request.content)
   return render(request, 'home.html',{'api':api})
